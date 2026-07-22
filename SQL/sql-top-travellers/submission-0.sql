47a15ceb7e3334789241092a1891dddb134e9d1f
Rabib001 <142938485+Rabib001@users.noreply.github.com>
2026-07-22 16:01:55 -0500

    Add: sql-top-travellers - submission-0

---
+++ SQL/sql-top-travellers/submission-0.sql
@@ -0,0 +1,5 @@
+select u.name, Coalesce(sum(r.distance),0) as travelled_distance
+from users u
+left join rides r on r.user_id=u.id
+group by u.id, u.name
+order by travelled_distance desc, u.name asc 
